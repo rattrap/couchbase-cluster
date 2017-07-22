41af4a14f9803295d36e30fcb6b4abbc9c4bb078
Leonard Mocanu <leonard.mocanu@ymail.com>
2017-07-22 22:41:05 +0300

    Fixed security group didn't allow machines to download stuff

--- infrastructure/modules/networking/main.tf
+++ infrastructure/modules/networking/main.tf
@@ -104,6 +104,20 @@ resource "aws_security_group" "default" {
     self      = true
   }
 
+  egress {
+    from_port   = "80"
+    to_port     = "80"
+    protocol    = "6"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = "443"
+    to_port     = "443"
+    protocol    = "6"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   tags {
     Name    = "${var.project} default security group"
     Project = "${var.project}"
